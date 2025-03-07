133b864bb78a9362f6bac82ac13e2a9261f735f6
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-03-07 14:13:43 -0300

    refactor transaction details screen to simplify layout and improve readability

--- app/(tabs)/transactions/[id].tsx
+++ app/(tabs)/transactions/[id].tsx
@@ -4,10 +4,6 @@ import { useLocalSearchParams } from "expo-router";
 import { useTransactions } from "./hooks/useTransactions";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { useRouter } from "expo-router";
-import { Badge } from "~/components/ui/badge";
-import { ArrowLeft } from "lucide-react-native";
-import { Separator } from "~/components/ui/separator";
-
 
 export default function TransactionDetailsScreen() {
     const { id } = useLocalSearchParams();
@@ -46,23 +42,16 @@ export default function TransactionDetailsScreen() {
 
     return (
         <View className="flex-1 p-4 bg-gray-100">
-            <TouchableOpacity onPress={() => router.back()} className="mb-4">
-                <ArrowLeft size={24} color="blue" />
-            </TouchableOpacity>
             <Card>
                 <CardHeader>
                     <CardTitle className="text-2xl font-bold text-gray-800">{transaction.name}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <Text className="text-lg text-gray-700">Categoria: <Badge variant="primary">{transaction.category.name}</Badge></Text>
-                    <Separator style={{ marginVertical: 8 }} />
-                    <Text className="text-lg text-gray-700">Tipo: <Badge variant={transaction.transaction_type === "income" ? "success" : "destructive"}>{transaction.transaction_type}</Badge></Text>
-                    <Separator style={{ marginVertical: 8 }} />
-                    <Text className="text-lg text-gray-700">Descrição: <Text className="font-semibold">{transaction.description || 'Nenhuma descrição disponível'}</Text></Text>
-                    <Separator style={{ marginVertical: 8 }} />
-                    <Text className="text-lg text-gray-700">Valor: <Text className="font-semibold">R$ {parseFloat(transaction.amount).toFixed(2)}</Text></Text>
-                    <Separator style={{ marginVertical: 8 }} />
-                    <Text className="text-lg text-gray-700">Data: <Text className="font-semibold">{new Date(transaction.created_at).toLocaleDateString()}</Text></Text>
+                    <Text className="text-lg text-gray-700">Categoria: <Text className="font-semibold">{transaction.category.name}</Text></Text>
+                    <Text className="text-lg text-gray-700 mt-2">Tipo: <Text className="font-semibold">{transaction.transaction_type}</Text></Text>
+                    <Text className="text-lg text-gray-700 mt-2">Descrição: <Text className="font-semibold">{transaction.description || 'Nenhuma descrição disponível'}</Text></Text>
+                    <Text className="text-lg text-gray-700 mt-2">Valor: <Text className="font-semibold">R$ {parseFloat(transaction.amount).toFixed(2)}</Text></Text>
+                    <Text className="text-lg text-gray-700 mt-2">Data: <Text className="font-semibold">{new Date(transaction.created_at).toLocaleDateString()}</Text></Text>
                 </CardContent>
                 <CardFooter className="flex justify-end mt-4">
                     <TouchableOpacity onPress={() => router.back()} className="bg-blue-500 py-2 px-6 rounded-full">
